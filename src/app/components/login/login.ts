5f48ce13405861dccc2a00cc0402460ce85fb663
Miguel Angel <miguelangelgarb@gmail.com>
2025-08-28 19:47:59 -0500

    Login v.1.1.0

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
-
+  email: string = '';
+  password: string = '';
 }
