2a457c12ed6a43ffd8fea767a56d18d20e8505d4
Miguel Angel <miguelangelgarb@gmail.com>
2025-08-28 19:09:16 -0500

    Login v.1.0.0

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadComponent: () => import('./components/login/login').then(m => m.Login) },
+  { path: 'register', loadComponent: () => import('./components/register/register').then(m => m.Register) }
+];
