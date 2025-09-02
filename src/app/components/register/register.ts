4754d97681d97dbecbc288517f2edc487aa367bd
Miguel Angel <miguelangelgarb@gmail.com>
2025-09-01 20:45:40 -0500

    Login v.1.1.2

--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { error } from 'console';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [CommonModule, FormsModule],
   standalone: true,
   templateUrl: './register.html',
   styleUrl: './register.css'
@@ -13,49 +15,57 @@ export class Register {
   user: string = '';
   email: string = '';
   cEmail: string = '';
-  private _password: string = '';
-  private _cPassword: string = '';
+  password: string = '';
+  cPassword: string = '';
 
-  get password(): string {
-    return this._password;
-  }
-
-  set password(value: string) {
-    this._password = value;
-  }
-
-  get cPassword(): string {
-    return this._cPassword;
-  }
-
-  set cPassword(value: string) {
-    this._cPassword = value;
-  }
-
-  register(): void {
+  register() {
 
     const regex = /^[a-zA-Z0-9._%+-]+@(gmail\.com|hotmail\.com|outlook\.com)$/;
 
     // Validación de campos.
-    if (!this.user || !this.email || !this.cEmail || !this.password || !this.cPassword) {
-      alert("Ingrese todos los campos solicitados.")
+    if (!this.user.trim() || !this.email.trim() || !this.cEmail.trim() || !this.password.trim() || !this.cPassword.trim()) {
+      alert("Ingrese todos los campos solicitados.");
       return;
     }
     // Validación de email.
     if (!regex.test(this.email)) {
-      alert("El email debe ser válido y pertenecer a un dominio.")
+      alert("El email debe ser válido y pertenecer a un dominio.");
       return;
     }
     // Validación de contraseña.
     if (this.password.length < 6) {
-      alert("La contraseña debe contener por lo menos 6 caracteres.")
+      alert("La contraseña debe contener por lo menos 6 caracteres.");
       return;
     }
     // Validación de confirmación de email y password.
     if (this.cEmail !== this.email || this.cPassword !== this.password) {
-      alert("El correo o contraseña no coinciden.")
+      alert("El correo o contraseña no coinciden.");
       return;
     }
+
+    fetch('http://127.0.0.1:8000', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        user: this.user,
+        email: this.email,
+        cEmail: this.cEmail,
+        password: this.password,
+        cPassword: this.cPassword
+      })
+    })
+
+      .then(response => response.json())
+      .then(data => {
+        console.log('Respuesta de la API:', data);
+        alert('Usuario creado con exito.');
+      }).catch(error => {
+        console.log('Error: ', error);
+        alert('Hubo un error creando el usuario.');
+      })
+
 
   }
 }
