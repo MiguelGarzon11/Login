6f8fdc6141e18e096e1e86ce8d0a09766782eafb
Miguel Angel <miguelangelgarb@gmail.com>
2025-09-02 17:36:10 -0500

    Login v.1.1.4

--- src/app/components/register/register.ts
+++ src/app/components/register/register.ts
@@ -23,37 +23,35 @@ export class Register {
     const regex = /^[a-zA-Z0-9._%+-]+@(gmail\.com|hotmail\.com|outlook\.com)$/;
 
     // Validación de campos.
-    if (!this.user.trim() || !this.email.trim() || !this.cEmail.trim() || !this.password.trim() || !this.cPassword.trim()) {
-      alert("Ingrese todos los campos solicitados.");
-      return;
-    }
-    // Validación de email.
-    if (!regex.test(this.email)) {
-      alert("El email debe ser válido y pertenecer a un dominio.");
-      return;
-    }
-    // Validación de contraseña.
-    if (this.password.length < 6) {
-      alert("La contraseña debe contener por lo menos 6 caracteres.");
-      return;
-    }
-    // Validación de confirmación de email y password.
-    if (this.cEmail !== this.email || this.cPassword !== this.password) {
-      alert("El correo o contraseña no coinciden.");
-      return;
-    }
+    // if (!this.user.trim() || !this.email.trim() || !this.cEmail.trim() || !this.password.trim() || !this.cPassword.trim()) {
+    //   alert("Ingrese todos los campos solicitados.");
+    //   return;
+    // }
+    // // Validación de email.
+    // if (!regex.test(this.email)) {
+    //   alert("El email debe ser válido y pertenecer a un dominio.");
+    //   return;
+    // }
+    // // Validación de contraseña.
+    // if (this.password.length < 6) {
+    //   alert("La contraseña debe contener por lo menos 6 caracteres.");
+    //   return;
+    // }
+    // // Validación de confirmación de email y password.
+    // if (this.cEmail !== this.email || this.cPassword !== this.password) {
+    //   alert("El correo o contraseña no coinciden.");
+    //   return;
+    // }
 
-    fetch('http://127.0.0.1:8000', {
+    fetch('http://127.0.0.1:8000/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        user: this.user,
+        username: this.user,
         email: this.email,
-        cEmail: this.cEmail,
-        password: this.password,
-        cPassword: this.cPassword
+        password: this.password
       })
     })
 
